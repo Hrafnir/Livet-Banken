41acd12597bd242447e03e95190912ba1e1a51a8
Hrafnir <eirik.gaarde@gmail.com>
2026-02-17 09:43:05 +0100

    Create data_housing.js

---
+++ data_housing.js
@@ -0,0 +1,84 @@
+/* Version: #1 - data_housing.js */
+const DATA_HOUSING = [
+    // --- TIL LEIE ---
+    {
+        id: "rent_basement",
+        type: "rent",
+        title: "Kjelleren til Mor & Far",
+        description: "Trygt, billig, men litt flaut. Lukter gammel katt.",
+        price: 2000, // Symbolsk sum for mat
+        comfort: -5, // Går ut over stoltheten
+        parking: true,
+        reqEquity: 0,
+        img: "" // Ingen bilde foreløpig
+    },
+    {
+        id: "rent_shared",
+        type: "rent",
+        title: "Rom i Kollektiv",
+        description: "Del kjøkken og bad med 4 andre. Fest hver helg.",
+        price: 6500,
+        comfort: -2, // Bråkete
+        parking: false,
+        reqEquity: 0,
+        img: ""
+    },
+    {
+        id: "rent_studio",
+        type: "rent",
+        title: "Sentrumsnær Hybel",
+        description: "15 kvm med eget bad og tekjøkken. Kort vei til alt.",
+        price: 9500,
+        comfort: 2,
+        parking: false,
+        reqEquity: 0,
+        img: ""
+    },
+    {
+        id: "rent_apartment",
+        type: "rent",
+        title: "Moderne 2-roms",
+        description: "Høy standard, balkong og heis.",
+        price: 14000,
+        comfort: 5,
+        parking: true,
+        reqEquity: 0,
+        img: ""
+    },
+
+    // --- TIL SALGS ---
+    {
+        id: "buy_fixer",
+        type: "buy",
+        title: "Oppussingsobjekt",
+        description: "Trenger mye kjærlighet. Perfekt for den nevenyttige.",
+        price: 1800000,
+        comfort: -1, // Støvete i starten
+        parking: true,
+        reqEquity: 0.15, // 15% krav
+        img: ""
+    },
+    {
+        id: "buy_apt_small",
+        type: "buy",
+        title: "Koselig Leilighet",
+        description: "Innflyttingsklar 2-roms i rolig strøk.",
+        price: 2900000,
+        comfort: 4,
+        parking: false,
+        reqEquity: 0.15,
+        img: ""
+    },
+    {
+        id: "buy_house",
+        type: "buy",
+        title: "Enebolig med Hage",
+        description: "God plass, garasje og hageflekk. Drømmeboligen.",
+        price: 5500000,
+        comfort: 10,
+        parking: true,
+        reqEquity: 0.15,
+        img: ""
+    }
+];
+/* Version: #1 */
